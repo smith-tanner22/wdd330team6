e29b0442a66479337b354dc99551adf1997a8403
Kevin <whi15032@byui.edu>
2022-04-27 12:22:41 -0600

    My name

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,9 +4,9 @@ function getLocalStorage(key) {
 
 function getCartContents() {
   // let markup = '';
-  const cartItems = getLocalStorage('so-cart');
+  const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
