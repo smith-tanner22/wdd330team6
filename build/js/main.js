0d232e8faeda628e6f1ebf76be3ab4dcb84fa7b9
N. Tanner Smith <smith.tanner2222@gmail.com>
2022-05-13 12:30:51 -0600

    finished team3

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+import t from"./productData.js";import o from"./productList.js";const a=new t,r=new o;console.table(r);
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import ProductListing from './productList';
+import ProductData from './productData';
+const head = document.querySelector('.product-list');
+const productData = new ProductData('tents');
+const productList = new ProductListing('tents', productData, head);
+
+console.table(productList);
+console.table(productData);
