27812cac7f55e746e597887395a0516d0850fb50
Kevin <whi15032@byui.edu>
2022-05-25 15:19:13 -0600

    Alert is functioning

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,36 +1,42 @@
 function convertToJson(res) {
-    if (res.ok) {
-      return res.json();
-    } else {
-      throw new Error("No alerts");
-    }
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("No alerts");
   }
+}
 
 export default class Alert {
-    constructor() {
-        this.path = '../json/alerts.json';
-        this.init();
-    }
-    getAlerts() {
-        return fetch(this.path)
-        .then(convertToJson)
-        .then((data) => data);
-    }
+  constructor() {
+    this.outputElement = document.getElementsByClassName("divider");
+    this.path = '../json/alerts.json';
+    this.init();
+  }
+  getAlerts() {
+      return fetch(this.path)
+      .then(convertToJson)
+      .then((data) => data);
+  }
 
-    async init() {
-        this.outputElement.innerHTML = this.alertTemplate;
-        const alerts = await this.getAlerts();
-        this.renderAlert(alerts, outputElement.innerHTML, ".alertList");
-    }
+  async init() {
+      const alerts = await this.getAlerts();
+      this.outputElement.innerHTML = this.alertTemplate();
+      // this.outputElement.querySelector(".divider") = alerts;
+      this.renderAlert(alerts, this.alertTemplate, ".alertList");
+  }
 
-    alertTemplate(){
-      return `<section class="alertList">
-        <p></p>
-      </section>`;
-    }
+  alertTemplate(alert){
+    return `<section class="alertList">
+      <p>${alert}</p>
+    </section>`;
+  }
 
-    renderAlert(alerts, template, outputId) {
-      
-    }
-    
+  renderAlert(alerts, template, outputClassName) {
+    const element = document.querySelector(".divider");
+    console.log(alerts[0].message);
+    element.innerHTML = " ";
+    const htmlString = alerts.map((item) => template(item.message));
+    element.innerHTML = htmlString.join("");
+  }
+  
 }
